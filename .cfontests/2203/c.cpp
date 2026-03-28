2a76322a6cda109f3a69d1df995c778615950261
SamuelCano03 <U202116508@upc.edu.pe>
2026-03-28 17:41:00 -0500

    2203

---
+++ .cfontests/2203/c.cpp
@@ -0,0 +1,131 @@
+/***--_saelcc03_--***/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include ".debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T>
+using ordered_multi_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define fori(i,n) for(int i=0;i<(int)n;i++)
+#define fore(i,n) for(int i=1;i<=(int)n;i++)
+#define fora(i,n) for(int i=(int)n-1;i>=0;i--)
+#define foro(i,a,b) for(int i=a;i<(int)b;i++)
+#define int long long
+#define inf 1e9
+#define INF 1e18
+#define pii pair<int,int>
+#define piii tuple<int,int,int>
+#define vi vector<int>
+#define vii vector<pii>
+#define viii vector<piii>
+#define vvi vector<vi>
+#define vvii vector<vii>
+#define vviii vector<viii>
+#define vvvi vector<vvi>
+#define vb vector<bool>
+#define vs vector<string>
+#define si set<int>
+#define mpii map<int,int>
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(x) (int)x.size()
+#define yesi ps("Yes")
+#define nosi ps("No")
+#define endl '\n'
+#define approx(a) fixed << setprecision(a)
+#define ff first
+#define ss second
+#define fast read(n); vi v(n); read(v)
+// reading your mind
+template <class T> void read(vector<T> &v);
+template <class F, class S> void read(pair<F, S> &p);
+template <class T> void read(T &x) {cin >> x;}
+template <class T> void read(vector<T> &v) {for(auto& x : v) read(x);}
+template <class R, class... T> void read(R& r, T&... t){read(r); read(t...);};
+template <class F, class S> void read(pair<F, S> &p) {read(p.ff, p.ss);}
+// puking your feelings
+template <class T> void ps(vector<T> &v);
+template <class F, class S> void pr(const pair<F, S> &x);
+template <class T> void pr(const T &x) {cout << x;}
+void ps() {pr("\n");}
+template <class R, class... T> void pr(const R& r, const T&... t) {pr(r); pr(t...);}
+template <class F, class S> void pr(const pair<F, S> &x) {pr("{", x.ff, ", ", x.ss, "}\n");}
+template <class T> void ps(vector<T> &v) {for(auto& x : v) pr(x, ' '); ps();}
+template <class T> void ps(set<T> &v) {for(auto& x : v) pr(x, ' '); ps();}
+template <class T> void ps(const T &x) {pr(x); ps();}
+template <class R, class... T> void ps(const R& r,  const T &...t) {pr(r, ' '); ps(t...);}
+
+int tc=1,n,m;
+int fx(int s, int m){
+  if(s==0)return 0;
+  int nm = m;
+  int ns = s%m;
+  while(nm>ns){
+    nm &= ~(1ll<<(63 - __builtin_clzll(nm)));
+  }
+  // dbg(ns,nm,s/m);
+  return s/m+fx(ns,nm);
+}
+void solve(int caso){
+  int s;
+  read(s,m);
+  int lsb = m&(-m);
+  if(s%lsb!=0)return ps(-1);
+  int nm = m;
+  vi v;
+  fori(i,63){
+    if((nm>>i)&1)v.pb(i);
+  }
+  sort(rall(v));
+  // dbg(v);
+  // dbg(m,s);
+  int lo = (m+s-1)/m, hi = s;
+  // dbg(lo,hi);
+  while(lo<hi){
+    n = (lo+hi)/2;
+    int srem = s;
+    for(int e: v){
+      int c = min(n,srem>>e);
+      srem -= c<<e;
+    }
+    // dbg(srem,lo,n,hi);
+    if(srem==0)hi = n;
+    else lo=n+1;
+  } 
+  ps(lo);
+}
+
+
+int32_t main(){
+  ios::sync_with_stdio(false); cin.tie(0);
+  read(tc); 
+  fore(caso, tc){
+    solve(caso);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
