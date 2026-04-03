235222c88a798e8be21070dd75b3f3e3d9d72261
saelcc03 <saelcc03@gmail.com>
2026-04-03 00:33:54 -0500

    asdf

--- .util/cf.sh
+++ .util/cf.sh
@@ -1,5 +1,5 @@
 if [ -z "$1" ]; then
-  echo "Ig you need an argument. Use contest id or name of contest"
+  echo "Ig you need an argument. Use contest id or name of contest 🤔"
   return 1
 fi
 
@@ -9,4 +9,4 @@ mv *.cpp .cfontests/"$1"/ 2>/dev/null
 
 $(dirname "$0")/setup.sh
 
-echo "Congrats! Now everyone knows you participated on $1"
+echo "Congrats! Now everyone knows you participated on $1 🤓"
